ab416e6463daceb329b077bdb4fea59b5f417a04
Pritam Mandal <pritammandal1717@gmail.com>
2025-02-03 15:33:36 +0530

    security fixes

--- server/src/Routes/userRoutes.ts
+++ server/src/Routes/userRoutes.ts
@@ -1,17 +1,26 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-
+import zod from 'zod'
 const userRoutes = new Hono<{
     Bindings : {
         DATABASE_URL : string
     }
 }>();
 
+const signUpBody = zod.object({
+    name : zod.string().min(1),
+    email : zod.string().email(),
+    password : zod.string().min(6),
+})
+
 userRoutes.post("/signup", (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
+
+
+
   return c.text("Hello World");
 });
 
