131228dff45df6d5e22f19f8fc4c78e2b2b7e15b
Pritam Mandal <pritammandal1717@gmail.com>
2025-02-04 14:51:46 +0530

    Authentication provided

--- server/src/Routes/userRoutes.ts
+++ server/src/Routes/userRoutes.ts
@@ -1,37 +1,119 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import zod from 'zod'
+import zod from "zod";
+import { sign } from "hono/jwt";
+import bcrypt from "bcryptjs";
+
 const userRoutes = new Hono<{
-    Bindings : {
-        DATABASE_URL : string
-    }
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
 }>();
 
 const signUpBody = zod.object({
-    name : zod.string().min(1),
-    email : zod.string().email(),
-    password : zod.string().min(6),
-})
+  name: zod.string().min(1),
+  email: zod.string().email(),
+  password: zod.string().min(6),
+});
 
 userRoutes.post("/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  await prisma.user.create({
-    data: {
-        name : "Pritam",
-        email : "pritam123@gmail.com",
-        password : "pritam@123"
-    }
-  })
+  const { success } = signUpBody.safeParse(await c.req.json());
 
-  return c.text("Hello World");
+  if (!success) {
+    c.status(400);
+    return c.json({ error: "Your input is too low" });
+  }
+  const body = await c.req.json();
+
+  try {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (existingUser) {
+      c.status(409);
+      return c.json({ error: "User already exist" });
+    }
+  } catch (err) {
+    return c.json({ error: "Somthing went wrong" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: hashedPassword,
+      },
+    });
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({ token });
+  } catch (err) {
+    return c.json({ error: "Somthing went wrong" });
+  }
 });
 
-userRoutes.post("/signin", (c) => {
-  return c.text("Hello World");
+const signInBody = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(6),
+});
+
+userRoutes.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const { success } = signInBody.safeParse(await c.req.json());
+
+  if (!success) {
+    c.status(400);
+    return c.json({ error: "Your input is too low" });
+  }
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (!user) {
+      c.status(400);
+      return c.json({ error: "User is not registered" });
+    }
+
+    try {
+      const isMatched = await bcrypt.compare(body.password, user.password);
+      if(!isMatched) {
+        c.status(401)
+        return c.json({error : "Please enter correct password"})
+      }
+
+      const token = await sign({id : user.id}, c.env.JWT_SECRET);
+      return c.json({token})
+
+    } catch (err) {
+      c.status(403)
+      return c.json({error : "Something went wrong", err})
+    }
+
+  } catch (err) {
+    return c.json({ error: "Somthing went wrong" });
+  }
+
 });
 
 export default userRoutes;
